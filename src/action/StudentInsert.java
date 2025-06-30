e4fcb9fc44797659de266ed2382cdd991d7dd895
y_baba <y_baba@LAPTOP-L3KDRRO9>
2025-06-30 13:56:18 +0900

    test

--- src/action/StudentInsert.java
+++ src/action/StudentInsert.java
@@ -18,6 +18,7 @@ import bean.Teacher;  // ログインユーザーのモデルクラス（必要�
 
 //aaaaa
 ///bbbb
+///cccc
 @WebServlet("/action/studentinsert")
 public class StudentInsert extends HttpServlet {
   protected void doPost(HttpServletRequest request, HttpServletResponse response)
